8b5465fa803b339f110a501f10663ff26ae5b41a
Tia Brown <anandabee01234@gmail.com>
2023-02-12 22:10:47 -0500

    modified changes: app.py

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from fastapi import FastAPI,Request
+from fastapi import FastAPI, Request
 from bson import ObjectId
 import motor.motor_asyncio
 import pydantic
+import os
+from pydantic import BaseModel, Field, EmailStr
+from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, HTTPException
-
+from fastapi import FastAPI, Body, Request, HTTPException, status
+from fastapi.responses import Response, JSONResponse
 from datetime import datetime
+from typing import Optional, List
 
 app = FastAPI()
 
 origins = [
-    "http://localhost.80000",
+    "http://localhost:8000",
     "https://ecse3038-lab3-tester.netlify.app"
 ]
 
@@ -29,56 +33,41 @@ pydantic.json.ENCODERS_BY_TYPE[ObjectId]=str
 
 @app.get("/profile")
 async def get_profile():
-    profile = await db["profile"].find().to_list(999)
-    if len(profile) < 1:
-        return {}
-    return profile[0]
+    profile = await db["Data"].find_one()
+    return profile
 
-@app.post("/profile",status_code=201)
-async def create_new_profile(request:Request):
-    
+@app.post("/profile", status_code=201)
+async def create_profile(request: Request):
     profile_object = await request.json()
     profile_object["last_updated"]=datetime.now()
 
-    new_profile = await db["profile"].insert_one(profile_object)
-    created_profile = await db["profile"].find_one({"_id": new_profile.inserted_id})
-
+    new_profile = await db["Data"].insert_one(profile_object)
+    created_profile = await db["Data"].find_one({"_id": new_profile.inserted_id})
     return created_profile
 
-@app.post("/data",status_code=201)
-async def create_new_profile(request:Request):
-    tank_object = await request.json()
-
-    new_tank = await db["tank"].insert_one(tank_object)
-    created_tank = await db["tank"].find_one({"_id": new_tank.inserted_id})
-
-    return created_tank
-    
 @app.get("/data")
-async def retrive_tanks():
-    tanks = await db["tank"].find().to_list(999)
-    return tanks
+async def get_all_data():
+    datas = await db["Waterdata"].find().to_list(999)
+    return datas
 
-@app.delete("/data/{id}",status_code=204)
-async def delete_tank(id: str):
+@app.post("/data", status_code=201)
+async def create_data(request: Request):
+    data_object = await request.json()
 
-    found= await db["tank"].find_one({"_id": ObjectId(id)})
-    if (found) is None:
-        raise HTTPException(status_code=404, detail="Item not found")
-
-    remove_tank= await db["tank"].delete_one({"_id":ObjectId(id)})
-
-    return {"message":"ITEM WAS DELETED "}
+    new_data = await db["Waterdata"].insert_one(data_object)
+    created_data = await db["Waterdata"].find_one({"_id": new_data.inserted_id})
+    return created_data
 
 @app.patch("/data/{id}")
-async def do_update(id:str, request: Request):
-    updated= await request.json()
-    result = await db["tank"].update_one({"_id":ObjectId(id)}, {'$set': updated})
-    
-    if result.modified_count == 1:
-         if (
-                updated_tank := await db["tank"].find_one({"_id": id})
-            ) is not None:
-                return updated_tank   
-    else:
-         raise HTTPException(status_code=404, detail="Item not found")
+async def update_data(id: str, request: Request):
+    info = await request.json()         
+    updated_data = await db["Waterdata"].update_one({"_id": ObjectId(id)}, {"$set": info})
+    result = await db["Waterdata"].find_one({"_id":id})
+    return result
+
+@app.delete("/data/{id}", status_code=204)        
+async def delete_data(id: str):
+    to_delete =  await db["Waterdata"].find_one_and_delete({"_id": ObjectId(id)}) 
+
+    if not to_delete:
+        raise HTTPException(status_code=404, detail="Not Found")
